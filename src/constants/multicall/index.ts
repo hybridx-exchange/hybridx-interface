f87ca6253e60e63e5084cb469f78235e55b67753
cherideal <101du@163.com>
2022-05-31 11:06:42 +0800

    op

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,9 +2,9 @@ import { ChainId } from '@hybridx-exchange/hybridx-sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.TESTNET]: '0x32Dd62d967335aed1480Cd7Da6B017F1874Af95f',
+  [ChainId.TESTNET]: '0xA0A9C353f250b7f26366a41083154B2c0B2854EE',
   [ChainId.MAINNET]: '',
-  [ChainId.OPTIMISM_TESTNET]: '0x466949b1df1f0a7121CD07f39aB2a921e2811283',
+  [ChainId.OPTIMISM_TESTNET]: '0xC6166de32636e851D960E4068707778fFb1C0389',
   [ChainId.OPTIMISM_MAINNET]: ''
 }
 
