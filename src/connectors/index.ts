c85656828d57ae4451a1ddb91162711329b7f9ce
cherideal <101du@163.com>
2022-06-16 09:39:47 +0800

    op

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -7,6 +7,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
 import { ChainId } from '@hybridx-exchange/hybridx-sdk'
+import getLibrary from '../utils/getLibrary'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
@@ -24,7 +25,7 @@ export const network = new NetworkConnector({
 
 let networkLibrary: Web3Provider | undefined
 export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
+  return (networkLibrary = networkLibrary ?? getLibrary(network.provider))
 }
 
 export const injected = new InjectedConnector({
--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -1,7 +1,31 @@
 import { Web3Provider } from '@ethersproject/providers'
+import ms from 'ms.macro'
+
+import { ChainId } from '@hybridx-exchange/hybridx-sdk'
+
+const NETWORK_POLLING_INTERVALS: { [chainId: number]: number } = {
+  [ChainId.MAINNET]: ms`1s`,
+  [ChainId.TESTNET]: ms`1s`,
+  [ChainId.OPTIMISM_MAINNET]: ms`1s`,
+  [ChainId.OPTIMISM_TESTNET]: ms`1s`
+}
 
 export default function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider)
-  library.pollingInterval = 15000
+  const library = new Web3Provider(
+    provider,
+    typeof provider.chainId === 'number'
+      ? provider.chainId
+      : typeof provider.chainId === 'string'
+      ? parseInt(provider.chainId)
+      : 'any'
+  )
+  library.pollingInterval = 15_000
+  library.detectNetwork().then(network => {
+    const networkPollingInterval = NETWORK_POLLING_INTERVALS[network.chainId]
+    if (networkPollingInterval) {
+      console.debug('Setting polling interval', networkPollingInterval)
+      library.pollingInterval = networkPollingInterval
+    }
+  })
   return library
 }
